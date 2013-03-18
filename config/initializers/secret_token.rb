73a66b0677d499fe1e12434ff2d8130e2c030306
Alex Hazoury <a.hazoury@gmail.com>
2013-03-18 12:41:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThamesCommute::Application.config.secret_token = '33a3cbb44711c2661158c05a11a05d04a9e163ca96003c9ed1c51ebd8f6fbd1724140fd31adfbcc860bb1d74073b6b63acd59eef04004c68c681ee703670c07c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ThamesCommute::Application.config.session_store :cookie_store, key: '_thames_commute_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ThamesCommute::Application.config.session_store :active_record_store
