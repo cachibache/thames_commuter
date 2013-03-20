79f4d4f3a28689683faa3362e51cbcfaa273c55c
railyboy <s.m.github@gmail.com>
2013-03-20 17:03:01 +0000

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+boats = Boat.create([ { name: "Johns Boat", slug: "johns-boat"}, 
+  { name: "Lady Marmaduke", slug: "lady-marmaduke"},
+  { name: "Sarah-Jane", slug: "sarah-jane"},
+  { name: "lazy lizzie", slug: "lazy-lizzie"},
+  { name: "New Janey", slug: "new-janey"},
+  { name: "The Tooth Fairy", slug: "the-tooth-fairy"}
+  ])
+
+passengers = Passenger.create!([{ name: "Jack The Ripper!!", slug: "jack-the-ripper-"},
+  { name: "Sally Who Met Harry", slug: "sally-who-met-harry"}
+  ])
