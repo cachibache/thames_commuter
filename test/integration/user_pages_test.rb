61c730278ea33b55e4b13815470e24b7e61956c4
railyboy <s.m.github@gmail.com>
2013-03-21 14:39:40 +0000

    Annotate tests and TDD User

--- test/integration/user_pages_test.rb
+++ test/integration/user_pages_test.rb
@@ -3,12 +3,30 @@ require 'test_helper'
 class UserPagesTest < ActionDispatch::IntegrationTest
   # factories :users
 
-  test "login and browse site" do
-    # login via https
-    https!
-    get "/login"
+  test "register user" do
+    get "/registration"
     assert_response :success
   end
+
+  test "registration page" do
+
+    subject { :page }
+
+    visit ('/registration')
+    fill_in('email', :with => 'BobsyourUncle')
+    click_button('Register')
+
+    # assert page.has
+
+  end
+
+
+  # test "login and browse site" do
+  #   # login via https
+  #   https!
+  #   get "/login"
+  #   assert_response :success
+  # end
 
 
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  name       :string(255)
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "User has the following symbols" do
+    @user = User.create(:name => "Bobs your Uncle", :email => 'bobsyouruncle@gmail.com')
+
+    assert_respond_to @user, :name
+    assert_respond_to @user, :email
+  end
 end
